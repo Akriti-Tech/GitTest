77fd55c657d93b091dd636267c32dd4d6345a5a8
Akriti-Tech <80493266+Akriti-Tech@users.noreply.github.com>
2021-03-12 15:05:39 -0600

    Add files via upload

---
+++ src/Automation/Automate.java
@@ -0,0 +1,8 @@
+package Automation;
+
+public class Automate {
+    public static void main(String[] args) {
+        System.out.println("Automation !!!");
+        System.out.println("Testing");
+    }
+}
