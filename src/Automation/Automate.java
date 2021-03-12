60d6bf9b2efc4ec86d19aa156267b8ab211931c6
akriti <akshrestha90@gmail.com>
2021-03-12 15:14:24 -0600

    print added

--- src/Automation/Automate.java
+++ src/Automation/Automate.java
@@ -3,8 +3,8 @@ package Automation;
 public class Automate {
     public static void main(String[] args) {
         System.out.println("Automation !!!");
-        System.out.println("Testing");
         System.out.println("Akriti Shrestha");
         System.out.println("This is testing");
+        System.out.println("Automation Testing");
     }
 }
